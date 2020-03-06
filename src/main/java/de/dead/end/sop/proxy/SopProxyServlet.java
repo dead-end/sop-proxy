92dc791fe032dd3b8c7ae0c99ef8ecbac11141f2
dead end <dead.end@inbox.ru>
2020-03-06 11:44:33 +0100

    proxy servlet

---
+++ src/main/java/de/dead/end/sop/proxy/SopProxyServlet.java
@@ -0,0 +1,31 @@
+package de.dead.end.sop.proxy;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.proxy.ProxyServlet;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+/**
+ * 
+ * @author dead-end
+ *
+ */
+public class SopProxyServlet extends ProxyServlet.Transparent {
+	private static final long serialVersionUID = 1L;
+
+	private final boolean useSsl;
+
+	public SopProxyServlet(final boolean useSsl) {
+		this.useSsl = useSsl;
+	}
+
+	@Override
+	protected HttpClient newHttpClient() {
+
+		if (useSsl) {
+			final SslContextFactory contextFactory = new SslContextFactory();
+			return new HttpClient(contextFactory);
+		}
+
+		return super.newHttpClient();
+	}
+}
