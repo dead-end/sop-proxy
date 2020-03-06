2daf5b173c1a3f19889ab1636fbdc8059169785e
dead end <dead.end@inbox.ru>
2020-03-06 13:29:23 +0100

    new keys

--- src/main/java/de/dead/end/sop/proxy/Constants.java
+++ src/main/java/de/dead/end/sop/proxy/Constants.java
@@ -1,6 +1,15 @@
 package de.dead.end.sop.proxy;
 
+/**
+ * 
+ * @author dead-end
+ *
+ */
 public interface Constants {
+
+	final static String CFG_PORT = "port";
+
+	final static String CFG_CSV = "csv";
 
 	final static String KEY_PROXY_TO = "proxyTo";
 
