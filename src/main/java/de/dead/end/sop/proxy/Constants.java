dc53d9783deff1ed6f7d4dc4eb6c98bb9ac903f5
dead end <dead.end@inbox.ru>
2020-03-06 11:46:12 +0100

    constants

---
+++ src/main/java/de/dead/end/sop/proxy/Constants.java
@@ -0,0 +1,10 @@
+package de.dead.end.sop.proxy;
+
+public interface Constants {
+
+	final static String KEY_PROXY_TO = "proxyTo";
+
+	final static String KEY_PREFIX = "prefix";
+
+	final static String HTTPS_PREFIX = "https://";
+}
