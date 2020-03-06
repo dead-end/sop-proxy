dc50354fbfa51091e130a736357710a3735300af
dead end <dead.end@inbox.ru>
2020-03-06 13:29:03 +0100

    initial version

---
+++ src/main/java/de/dead/end/sop/proxy/Configs.java
@@ -0,0 +1,55 @@
+package de.dead.end.sop.proxy;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * 
+ * @author dead-end
+ *
+ */
+public class Configs {
+
+	private final Properties props;
+
+	/**
+	 * The constructor loads the configurations from a property file.
+	 */
+	public Configs(final String filename) throws IOException {
+
+		try (final InputStream is = new FileInputStream(filename)) {
+			this.props = new Properties();
+			this.props.load(is);
+		}
+	}
+
+	/**
+	 * The method returns a String property from the configuration file.
+	 */
+	public String getString(final String key) {
+		ensure(key);
+
+		return this.props.getProperty(key);
+	}
+
+	/**
+	 * The method returns an int property from the configuration file.
+	 */
+	public int getInt(final String key) {
+		ensure(key);
+
+		return Integer.parseInt(this.props.getProperty(key));
+	}
+
+	/**
+	 * The method ensures that the properties file contains a given key.
+	 */
+	private void ensure(final String key) {
+
+		if (!props.containsKey(key)) {
+			throw new IllegalArgumentException(String.format("Key not found: %s", key));
+		}
+	}
+}
