c364e397075629220caab901b68dd2f61966212b
dead end <dead.end@inbox.ru>
2020-03-08 12:42:09 +0100

    Initial version

--- src/main/java/de/dead/end/sop/proxy/ParserException.java
+++ src/main/java/de/dead/end/sop/proxy/ParserException.java
@@ -10,10 +10,10 @@ public class ParserException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * The constructor is called with the line and the line number which causes that
-	 * problem.
+	 * The constructor is called with the parser, which contains the line and the
+	 * line number which causes that problem.
 	 */
-	public ParserException(final int count, final String line, final String format, Object... args) {
-		super(String.format("line: %d '%s' - %s", count, line, String.format(format, args)));
+	public ParserException(final Parser parser, final String format, Object... args) {
+		super(String.format("line: %d '%s' - %s", parser.getCount(), parser.getLine(), String.format(format, args)));
 	}
 }
